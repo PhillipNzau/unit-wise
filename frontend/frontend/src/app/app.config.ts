290669860d816b8c6f4112969edead1c346eeff0
phillipnzau <nzauonline@gmail.com>
2025-09-10 19:22:17 +0300

    feat: tailwind setup and pwa init

--- frontend/frontend/src/app/app.config.ts
+++ frontend/frontend/src/app/app.config.ts
@@ -8,12 +8,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHotToastConfig } from '@ngneat/hot-toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHotToastConfig({
+      visibleToasts: 1,
+      duration: 700,
+      position: 'bottom-center',
+    }),
     provideServiceWorker('ngsw-worker.js', {
       enabled: true,
       registrationStrategy: 'registerWhenStable:30000',
